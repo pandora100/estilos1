deaae48b4693429dab5bdf9437e27d94b939dbed
pandora100 <rramossh1@gmail.com>
2020-09-11 20:24:03 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
-import "./style.css";
 
+import React,{useEffect, useRef, useState} from "react";
+import "./style.css";
+//
 export default function App() {
+   const [hidden,setHidden]= useState(false);
+  const buttonRef = useRef(null);
+  const buttonTextRef = useRef(null);
+  const moreRef = useRef(null);
+
+  useEffect(() => {
+        // *** After render, don't do anything, just remember we've seen the render
+        if (hidden && moreRef.current) {
+            console.log("hidden false");
+            moreRef.current.className = 'escondido';
+            buttonTextRef.current.innerText = 'Show less';
+            buttonTextRef.current.nextElementSibling.setAttribute("data-icon", "caret-up");
+        } else if(!hidden && moreRef.current) {
+            console.log("hidden true");
+            moreRef.current.className = 'aparecido';
+            buttonTextRef.current.innerText = 'Show More';
+            buttonTextRef.current.nextElementSibling.setAttribute("data-icon", "caret-down");
+
+        }
+    }, [hidden]);
+  const onclick = () => { 
+  setHidden(!hidden);
+};
+
+
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <React.Fragment>
+    <button ref={buttonRef} onClick={onclick} className="Label">
+    <span ref={buttonTextRef}>Show More</span>
+    <i  className="fas fa-caret-down"></i>
+  </button>
+
+  <p ref={moreRef}
+      
+           className="Label--info">Seleccione el Modulo además del Grupo ó Tecnología</p>
+    
+  
+  </React.Fragment>
   );
 }
+
+
+
